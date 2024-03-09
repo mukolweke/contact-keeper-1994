24dd9447e19f1a161c2530aad4880f124f381979
Michael Mukolwe <michaelolukaka@gmail.com>
2024-03-09 06:47:17 +0300

    Contact Keeper - setup routes

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+// routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/contacts', require('./routes/contacts'));
 app.get('/', (req, res) => res.json({ msg: 'Hello World Contact Keeper' }))
 
 const PORT = process.env.PORT || 5000
