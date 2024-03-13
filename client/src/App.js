47e6560291d7f5f0390f7de3dd9f98bcdff05f16
Michael Mukolwe <michaelolukaka@gmail.com>
2024-03-13 16:59:10 +0300

    Contact keeper - add alert context and alert compnent

--- client/src/App.js
+++ client/src/App.js
@@ -3,33 +3,39 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ContactState from './context/contact/ContactState'
 import AuthState from './context/auth/AuthState'
+import AlertState from './context/alert/AlertState'
 
 import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
 import Register from './components/auth/register'
 import Login from './components/auth/login'
+import Alerts from './components/layout/Alerts'
 
 const App = () => {
 	return (
-		<AuthState>
-			<ContactState>
-				<Router>
-					<Fragment>
-						<Navbar />
+		<AlertState>
+			<AuthState>
+				<ContactState>
+					<Router>
+						<Fragment>
+							<Navbar />
 
-						<div className='container'>
-							<Routes>
-								<Route exact path='/' element={<Home />}></Route>
-								<Route exact path='/about' element={<About />}></Route>
-								<Route exact path='/login' element={<Login />}></Route>
-								<Route exact path='/register' element={<Register />}></Route>
-							</Routes>
-						</div>
-					</Fragment>
-				</Router>
-			</ContactState>
-		</AuthState>
+							<div className='container'>
+								<Alerts />
+								
+								<Routes>
+									<Route exact path='/' element={<Home />}></Route>
+									<Route exact path='/about' element={<About />}></Route>
+									<Route exact path='/login' element={<Login />}></Route>
+									<Route exact path='/register' element={<Register />}></Route>
+								</Routes>
+							</div>
+						</Fragment>
+					</Router>
+				</ContactState>
+			</AuthState>
+		</AlertState>
 	)
 }
 
